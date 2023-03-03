f160a087f9de105daa14c39d7399a0c5273efc76
james <james@pui-Macmini.local>
2023-03-03 15:41:17 +0900

    refactoring

--- examples/terraform-ec2-bastion/versions.tf
+++ examples/terraform-ec2-bastion/versions.tf
@@ -18,7 +18,3 @@ terraform {
 provider "aws" {
   region  = var.aws_region
 }
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
